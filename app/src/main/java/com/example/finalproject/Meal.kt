0676c15801e4c932cd12f4c0f7a66065a7a5d99d
Lyutikov Nikita <nik-kaliningrad@mail.ru>
2019-02-28 14:53:40 +0200

    Little changes

--- app/src/main/java/com/example/finalproject/Meal.kt
+++ app/src/main/java/com/example/finalproject/Meal.kt
@@ -1,14 +1,12 @@
 package com.example.finalproject
 
-import retrofit2.http.Url
-
 data class Meal (
     val name: String,
     val category: String,
     val area: String,
     val instructions: String,
-    val mealThum: Url,
+    val mealThum: String,
     val tags: String,
-    val youTube: Url,
+    val youTube: String,
     val ingredients: List<Ingradient>
 )
