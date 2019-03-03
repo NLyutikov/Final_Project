d7ede69be62796625e1131357727bc4b838600d7
ll.mix@ya.ru <ll.mix@ya.ru>
2019-03-03 14:56:52 +0300

    filter

--- app/src/main/java/com/example/finalproject/APIServise.kt
+++ app/src/main/java/com/example/finalproject/APIServise.kt
@@ -6,4 +6,10 @@ import retrofit2.http.GET
 interface APIServise {
     @GET("api/json/v1/1/random.php")
     fun getRandomMeal(): Call<List<Meal>>
+
+    @GET("list.php?i=list")
+    fun getIngradients(): Call<RemoteResponse<Ingredient>>
 }
+
+
+data class RemoteResponse<T>(val meals: List<T>)
--- app/src/main/java/com/example/finalproject/Ingradient.kt
+++ app/src/main/java/com/example/finalproject/Ingradient.kt
@@ -1,6 +1,7 @@
 package com.example.finalproject
 
 data class Ingradient (
+    val id: Int,
     val name: String,
     val measure: String
 )
