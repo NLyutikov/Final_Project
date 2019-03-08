dfab8187a6bc421ae9d2f08a8d718c279fea35fe
Lyutikov Nikita <nik-kaliningrad@mail.ru>
2019-03-08 09:11:12 +0200

    Changed api random meals to normal

--- app/src/main/java/com/example/finalproject/ApiManager.kt
+++ app/src/main/java/com/example/finalproject/ApiManager.kt
@@ -1,9 +1,10 @@
 package com.example.finalproject
 
-import androidx.fragment.app.FragmentActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.*
 
 private const val BASE_URL = "https://www.themealdb.com/"
 
@@ -18,23 +19,17 @@ object ApiManager {
     }
 
     fun getRandomMeals(
-        activity: FragmentActivity?,
-        quantity: Int,
-        onSuccess: ((LinkedList<MealNetwork>) -> Unit),
+        onSuccess: ((List<MealNetwork>) -> Unit),
         onFailure: (String) -> Unit
     ) {
-        Thread {
-            val list = LinkedList<MealNetwork>()
-            for (i in 0 until quantity) {
-                val response = apiServise.getRandomMeal().execute()
-                if (response.isSuccessful) {
-                    list.add(response.body()!!.meals[0])
-                } else {
-                    activity?.runOnUiThread { onFailure.invoke(response.errorBody()!!.string()) }
-                    break
-                }
+        apiServise.getRandomMeal().enqueue(object : Callback<MealsNetwork> {
+            override fun onFailure(call: Call<MealsNetwork>, t: Throwable) {
+                onFailure.invoke(t.toString())
             }
-            activity?.runOnUiThread { onSuccess.invoke(list) }
-        }.start()
+
+            override fun onResponse(call: Call<MealsNetwork>, response: Response<MealsNetwork>) {
+                onSuccess.invoke(response.body()!!.meals)
+            }
+        })
     }
 }
